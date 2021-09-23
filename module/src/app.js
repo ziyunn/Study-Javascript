0a5171209190f5e5fb5e326f9bc4a03d2f8ca726
ziyun <jykim306@gmail.com>
2021-09-23 14:16:31 +0900

    모듈화

---
+++ module/src/app.js
@@ -0,0 +1,11 @@
+import Items from "./components/Items.js";
+
+class App {
+    constructor() {
+        const $app = document.querySelector('#app');
+        new Items($app);
+    };
+    
+}
+
+new App();
